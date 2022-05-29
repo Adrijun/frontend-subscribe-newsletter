8c2eb61b8096d79190bb937e9f8a6051c125f3ab
Jonas <jonasericsson90@hotmail.com>
2022-05-29 22:45:51 +0200

    styled loggedin

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,122 @@
-import React, { ChangeEvent } from 'react';
-import './App.css';
-import { useState } from "react"
-import axios from 'axios';
+import React, { ChangeEvent } from "react";
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+import { request, ServerResponse } from "http";
+import { Http2ServerRequest } from "http2";
 
 function App() {
-
   interface InewUser {
-    username: String,
-    password: String,
-    subscribe: boolean
+    username: String;
+    password: String;
+    subscribe: boolean;
   }
 
-  // const [username, setusername] = useState('');
-  // const [password, setpassword] = useState('');
-  
-  // let handleSubmit = async () =>{
-  //   try{
-  //     let res = await fetch('http://localhost:3000/users/add', {
-  //       method: 'POST',
-  //       body: JSON.stringify({
-  //         username: username,
-  //         password: password,
-  //       }),
-  //     });
-  //     if (res.status === 200){
-  //       console.log('rätt');
-  //     } else {
-  //       console.log('fel');
-  //     }
-  //   } catch (err){
-  //     console.log(err); 
-  //   }
-  // };
-  
-  const [newUser, setNewUser] = useState<InewUser>({username:'', password:'', subscribe:false})
+  const [newUser, setNewUser] = useState<InewUser>({
+    username: "",
+    password: "",
+    subscribe: false,
+  });
+  const [logginUser, setLoggin] = useState<InewUser>({
+    username: "",
+    password: "",
+    subscribe: false,
+  });
 
-  console.log(newUser);
-  
   // pushar till backend -> api
-  function sendToApi(){
-    axios.post<InewUser>('http://localhost:3000/users/add', newUser)
-    .then(res =>{
-      console.log(res, 'Rätt');
-    }).catch(err =>{
-      console.log(err, 'Fel');
-    });
+  function sendToApi() {
+    axios
+      .post<InewUser[]>("http://localhost:3000/users/add", newUser)
+      .then((res) => {
+        console.log(res, "Rätt");
+      })
+      .catch((err) => {
+        console.log(err, "Fel");
+      });
   }
 
-  // använder oss av inputfällt
-function handleChange(e:ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>){
-  let name:string = e.target.name;
+  // använder oss av inputfällt i create user
+  function handleChange(
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) {
+    let name: string = e.target.name;
 
-  if(name == 'username' || name == 'password'){ 
-    setNewUser({...newUser, [name]: e.target.value});
-}
-  else{
-    console.log('Det blev fel');
-}
-}
+    if (name == "username" || name == "password") {
+      setNewUser({ ...newUser, [name]: e.target.value });
+    } else {
+      console.log("Det blev fel");
+    }
+  }
+
+  // loggin funktion
+
+  interface Iusers {
+    _id: string;
+    username: String;
+    password: String;
+    subscribe: boolean;
+  }
+
+  // testning av loggin (fungerar inte än)
+  const [error, setError] = useState(null);
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoggin = () => {
+    axios
+      .post("http://localhost:3000/users/loggin", {
+        username: username,
+        password: password,
+      })
+      .then((response) => {
+        console.log("response", response);
+      })
+      .catch((error) => {
+        console.log("err", error);
+      });
+  };
 
   return (
     <div className="App">
-   
-   <h1>Loggin</h1>
-   <label htmlFor="username">username</label><br />
-   <input type="text" name='username' /><br />
-   <label htmlFor="password">password</label><br />
-   <input type="text" name='password' /><br />
-   <button>skicka</button><br />
+      <h1>Loggin</h1>
 
-<h1>create user</h1>
+      <form>
+        <label htmlFor="username">username</label>
+        <br />
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <br />
+        <label htmlFor="password">password</label>
+        <br />
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button onClick={handleLoggin}>skicka</button>
+        <br />
 
-<form>
-   <label htmlFor="username">user</label><br />
-   <input type="text" name='username' onChange={handleChange}
-   />
-   <br />
-   <label htmlFor="password">password</label><br />
-   <input type="text" name='password' onChange={handleChange} 
-   />
-   <br />
-   <button onClick={sendToApi}>skapa</button>
-   </form>
+        {error && <p className="error">{error}</p>}
+      </form>
+
+      <h1>Create User</h1>
+
+      <form>
+        <label htmlFor="username">user</label>
+        <br />
+        <input type="text" name="username" onChange={handleChange} />
+        <br />
+        <label htmlFor="password">password</label>
+        <br />
+        <input type="text" name="password" onChange={handleChange} />
+        <br />
+        <button onClick={sendToApi}>skapa</button>
+      </form>
     </div>
-    
   );
 }
 
--- src/components/loggedin.tsx
+++ src/components/loggedin.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import "./loggedin.css";
+
 export function LoggedIn() {
+  const [checked, setChecked] = useState(false);
+  console.log(checked);
+
   return (
     <>
-      <h1>HEJ!</h1>;
+      <section>
+        <h2>Du är inloggad!</h2>
+        <h3>Vill du prenumerera på vårat nyhetsbrev?</h3>
+        <input
+          type="checkbox"
+          id="checkbox"
+          defaultChecked={checked}
+          onChange={() => setChecked(!checked)}
+        />
+      </section>
     </>
   );
 }
